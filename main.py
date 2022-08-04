cc22aeddb3fbb34f9e766da4307561dcd9b00115
David Venegas <venegasdavidm@gmail.com>
2022-08-04 16:20:36 -0400

    yum

--- main.py
+++ main.py
@@ -1,4 +1,25 @@
+from email.message import EmailMessage
+from multiprocessing import context
 import smtplib
-server = smtplib.SMTP('smtp.gmail.com', 587)
-server.starttls()
-server.login('venegasdavidm@gmail.com', 's3cureP@ssw0rd')
+import ssl
+
+email_sender = 'spotifypodcastbot@gmail.com'
+email_password = 'jornxxaujitbnmsa'
+email_reciver = 'venegasdavidm@gmail.com'
+
+subject = "TEST SUBJECT"
+body = """
+    THIS IS A TEST BODY
+"""
+
+em = EmailMessage()
+em['From'] = email_sender
+em['To'] = email_reciver
+em['subject'] = subject
+em.set_content(body)
+
+context = ssl.create_default_context()
+
+with smtplib.SMTP_SSL('smtp.gmail.com', 465, context=context) as smtp:
+    smtp.login(email_sender, email_password)
+    smtp.sendmail(email_sender, email_reciver, em.as_string())
