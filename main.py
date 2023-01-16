a212d306e8ddcb9c62393d21eba002bae0322711
David Venegas <venegasdavidm@gmail.com>
2023-01-16 03:59:28 -0500

    issues with the stupi spotify api

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import smtplib
+from smtplib import SMTP
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
+from spotipy import Spotify, SpotifyOAuth, SpotifyOauthError
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 
 
@@ -25,7 +26,7 @@ def send_email(**email_variables):
     HTML_BODY = MIMEText(BODY, 'html')
     MESSAGE.attach(HTML_BODY)
 
-    server = smtplib.SMTP('smtp.gmail.com:587')
+    server = SMTP('smtp.gmail.com:587')
     server.starttls()
     server.login(email_sender, email_password)
     server.sendmail(email_sender, email_receiver, MESSAGE.as_string())
@@ -35,10 +36,31 @@ def send_email(**email_variables):
 
 
 def get_spotify_data():
-    return {
-        'saved_episodes': '44',
-        'new_episodes': '99',
-    }
+    client_id = '4571820e8fd04623b66368f248ba2ef5'
+    client_secret = '0e6dc1815d5b4ac2aab3a2f88273cb13'
+    redirect_uri = 'http://localhost:8888/callback'
+    scopes = 'user-library-read'
+
+    sp = Spotify(
+        auth_manager=SpotifyOAuth(
+            client_id=client_id,
+            client_secret=client_secret,
+            redirect_uri=redirect_uri,
+            scope=scopes,
+        )
+    )
+
+    while True:
+        try:
+            data = sp.current_user_saved_shows()
+            return data
+        except SpotifyOauthError as e:
+            sp = Spotify(auth_manager=SpotifyOAuth(scope=scopes))
+
+    # return {
+    #     'saved_episodes': '44',
+    #     'new_episodes': '99',
+    # }
 
 
 def run_bot():
